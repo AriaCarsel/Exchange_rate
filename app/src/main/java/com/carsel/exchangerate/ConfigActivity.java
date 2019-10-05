c458c852c6cdef3a31525e58026ce8063d5aa6d2
3311460487 <email@3311460487.com>
2019-10-05 13:41:55 +0800

    新增功能：实时获取汇率

--- app/src/main/java/com/carsel/exchangerate/ConfigActivity.java
+++ app/src/main/java/com/carsel/exchangerate/ConfigActivity.java
@@ -1,6 +1,9 @@
 package com.carsel.exchangerate;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,13 +18,16 @@ import java.text.DecimalFormat;
 public class ConfigActivity extends AppCompatActivity {
     Button save;
 
-    double dollar1,euro1,won1;
+    float dollar1,euro1,won1;
     EditText text1,text2,text3;
     String dollar2,euro2,won2;
+    SharedPreferences sp;
+    String TAG;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.config);
+        TAG="TAG";
 
         save = findViewById(R.id.button_save);
         text1 = findViewById(R.id.editText2);
@@ -31,9 +37,14 @@ public class ConfigActivity extends AppCompatActivity {
         Intent intent = getIntent();//获取intent
         Bundle bdl = intent.getExtras();//获取Bundle
         if(bdl!=null) {//如果不为空则提前数据
-            dollar1 = bdl.getDouble("key_dollar", 0.0);//获取原来的数据
-            euro1 = bdl.getDouble("key_euro", 0.0);
-            won1 = bdl.getDouble("key_won", 0.0);
+            dollar1 = bdl.getFloat("key_dollar", 0.0f);//获取原来的数据
+            euro1 = bdl.getFloat("key_euro", 0.0f);
+            won1 = bdl.getFloat("key_won", 0.0f);
+
+            Log.i(TAG,"Bundle--------------------------------------------");
+            Log.i(TAG,"the dollar_rate get from config is = "+dollar1);
+            Log.i(TAG,"the euro_rate get from config is = "+euro1);
+            Log.i(TAG,"the won_rate get from config is = "+won1);
         }
 
         text1.setText(String.valueOf(dollar1));//重置数据
@@ -53,9 +64,9 @@ public class ConfigActivity extends AppCompatActivity {
             Toast.makeText(this, "please input the exchange!", Toast.LENGTH_SHORT).show();
         }
         else {//检查是否输入0汇率
-              dollar1=Double.parseDouble(dollar2);
-              euro1=Double.parseDouble(euro2);
-              won1=Double.parseDouble(won2);
+              dollar1=Float.parseFloat(dollar2);
+              euro1=Float.parseFloat(euro2);
+              won1=Float.parseFloat(won2);
               if(dollar1==0||euro1==0||won1==0){//如果输入汇率为0
                   Toast.makeText(this, "The exchange is zero!", Toast.LENGTH_SHORT).show();
               }
@@ -63,9 +74,21 @@ public class ConfigActivity extends AppCompatActivity {
               else{//如果输入的汇率一切正确
                   Intent intent = getIntent();
                   Bundle bdl = new Bundle();
-                  bdl.putDouble("key_dollar",dollar1);
-                  bdl.putDouble("key_euro",euro1);
-                  bdl.putDouble("key_won",won1);
+                  sp = getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+
+                  SharedPreferences.Editor editor = sp.edit();
+                  editor.putFloat("dollar_rate",dollar1);
+                  editor.putFloat("euro_rate",euro1);
+                  editor.putFloat("won_rate",won1);
+                  editor.apply();
+                  Log.i(TAG,"myrate--------------------------------------------");
+                  Log.i(TAG,"the dollar_rate put in myrate is = "+dollar1);
+                  Log.i(TAG,"the euro_rate put in myrate is = "+euro1);
+                  Log.i(TAG,"the won_rate put in myrate is = "+won1);
+
+                  bdl.putFloat("key_dollar",dollar1);//注意存放的类型
+                  bdl.putFloat("key_euro",euro1);//如果存放与接收时使用的方法不同，则即使关键词一样也找不到
+                  bdl.putFloat("key_won",won1);
                   intent.putExtras(bdl);
                   setResult(2,intent);//设置resultCode及带回的数据
                   finish();//返回原来的页面
--- app/src/main/java/com/carsel/exchangerate/MainActivity.java
+++ app/src/main/java/com/carsel/exchangerate/MainActivity.java
@@ -1,9 +1,11 @@
 package com.carsel.exchangerate;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
-import android.opengl.ETC1;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,35 +13,126 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.DecimalFormat;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DecimalFormat;
 import static android.widget.Toast.LENGTH_LONG;
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Runnable {
     EditText editText;
     TextView textView;
     Button btn1,btn2,btn3;
-    String money;
-    double dollarRate,euroRate,wonRate,res;
+    String money,TAG;
+    float dollarRate,euroRate,wonRate,res;
+
+    Handler handler;//管理不同线程之间的消息，协助线程消息传递
+    Message msg;//线程之间消息实例
     DecimalFormat df;
-    String TAG="TAG";
-    @Override
+    SharedPreferences sharedPreferences;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //创建实例
+        TAG="TAG";
         editText = findViewById(R.id.editText1);
         textView = findViewById(R.id.textView1);
         btn1 = findViewById(R.id.button1);
         btn2 = findViewById(R.id.button2);
         btn3 = findViewById(R.id.button3);
         df = new DecimalFormat("#.00");
+        sharedPreferences = getSharedPreferences("myrate", Activity.MODE_PRIVATE);//第一种写法
+        // 获取上次修改的数据
+        if(sharedPreferences!=null) {//如果不为空则修改
+            dollarRate = sharedPreferences.getFloat("dollar_rate", 0.1405f);
+            euroRate = sharedPreferences.getFloat("euro_rate", 0.1279f);
+            wonRate = sharedPreferences.getFloat("won_rate", 167.8282f);
+            Log.i(TAG,"myrate--------------------------------------------");
+            Log.i(TAG,"the dollar_rate from myrate is = "+dollarRate);
+            Log.i(TAG,"the euro_rate from myrate is = "+euroRate);
+            Log.i(TAG,"the won_rate from myrate is = "+wonRate);
+        }
 
-        //初始汇率
-        dollarRate=0.1405;
-        euroRate=0.1279;
-        wonRate=167.8282;
-    }
+        //收快递
+        handler = new Handler(){//生命周期为窗口的打开到关闭
+            public void handleMessage(Message mesg){//时刻监测
 
-    public void Dollar(View view){
+                if(mesg!=null&&mesg.what==5){//检查发送过来的快递单号
+                    String res=mesg.obj.toString();//获得HTML
+
+
+                    Document doc = Jsoup.parse(res);
+                    Elements trs = doc.select("table").select("tr"); // 关键的一步 从html中把课表解析出来
+
+                    for (int i = 1; i < trs.size(); i++) {
+                        Elements tds = trs.get(i).select("td");
+                        String name=tds.get(0).text().trim();//获取名称
+                        if(!name.equals("美元")&&!name.equals("欧元")&&!name.equals("韩币")){//如果不是需要的币种
+                            continue;//跳出本次循环
+                        }
+                        else {
+                            for(int j=1;j<tds.size();j++) {
+                                String x = tds.get(j).text().trim();//获取汇率
+                                if(x.equals("--"))//如果当前汇率为空
+                                    continue;
+                                else {
+                                    switch(name) {
+                                        case "美元" : dollarRate=100/(Float.parseFloat(x));break;
+                                        case "欧元" : euroRate=100/(Float.parseFloat(x));break;
+                                        case "韩币" : wonRate=100/(Float.parseFloat(x));break;
+                                        default : break;
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+
+               Log.i(TAG,"美元汇率为："+dollarRate);
+               Log.i(TAG,"欧元汇率为："+euroRate);
+               Log.i(TAG,"美元汇率为："+wonRate);
+                //处理结束
+                }
+                super.handleMessage(mesg);//标配语句，不用理解但是必须由
+            }
+        };//Handel结束
+        Thread thread = new Thread(this);//线程初始化，注意线程的初始化应该在Handel后面
+        thread.start();
+
+
+    }//onCreate方法结束
+
+    public void run(){ //子线程开启
+        // 获取网络数据
+        URL url;
+        try {
+            url = new URL("http://www.usd-cny.com/icbc.htm");//在调试时一定要记得把手机的网打开
+            Log.i(TAG,"run:url="+url);
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            InputStream in = http.getInputStream();
+            String html = inputStream2String(in);
+
+            if(html!=null){
+//                Log.i(TAG,"html="+html);
+                msg = handler.obtainMessage(5);//填写快递单号
+                msg.obj = html;//快递内容
+                handler.sendMessage(msg);//发送
+                //将HTML发过去
+            }
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }//run方法结束
+
+    public void Dollar(View view){//Dollar
         money=editText.getText().toString();
         if(money==null||money.length()==0||money.equals("0")){//如果输入不规范，则提示输入错误
             Toast.makeText(this,"please input correct RMB",LENGTH_LONG).show();
@@ -47,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
             editText.setText("");//同时清空
         }
         else{//输入有效
-            res=Double.parseDouble(money);
+            res=Float.parseFloat(money);
             if(dollarRate==0){
                 textView.setText("exchange is null!");//汇率为空
                 editText.setText("");//同时清空
@@ -60,8 +153,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-//    EURO
-    public void Euro(View view){
+    public void Euro(View view){//    EURO
         money=editText.getText().toString();
         if(money==null||money.length()==0||money.equals("0")){//如果输入不规范，则提示输入错误
             Toast.makeText(this,"please input correct RMB",LENGTH_LONG).show();
@@ -69,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
             editText.setText("");//同时清空
         }
         else{//输入有效
-            res=Double.parseDouble(money);
+            res=Float.parseFloat(money);
             if(euroRate==0){
                 textView.setText("exchange is null!");//汇率为空
                 editText.setText("");//同时清空
@@ -82,8 +174,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-//WON
-    public void Won(View view){
+    public void Won(View view){//WON
         money=editText.getText().toString();
         if(money==null||money.length()==0||money.equals("0")){//如果输入不规范，则提示输入错误
             Toast.makeText(this,"please input correct RMB",LENGTH_LONG).show();
@@ -91,7 +182,7 @@ public class MainActivity extends AppCompatActivity {
             editText.setText("");//同时清空
         }
         else{//输入有效
-            res=Double.parseDouble(money);
+            res=Float.parseFloat(money);
             if(wonRate==0){
                 textView.setText("exchange is null!");//汇率为0
                 editText.setText("");//同时清空
@@ -109,22 +200,45 @@ public class MainActivity extends AppCompatActivity {
         config = new Intent(this, ConfigActivity.class);//打开config页面
         //装入Bundle
         Bundle bdl = new Bundle();
-        bdl.putDouble("key_dollar",dollarRate);
-        bdl.putDouble("key_euro",euroRate);
-        bdl.putDouble("key_won",wonRate);
+        bdl.putFloat("key_dollar",dollarRate);
+        bdl.putFloat("key_euro",euroRate);
+        bdl.putFloat("key_won",wonRate);
+        Log.i(TAG,"Bundle--------------------------------------------");
+        Log.i(TAG,"the dollar_rate pushed in  Bundle is = "+dollarRate);
+        Log.i(TAG,"the euro_rate pushed in Bundle is = "+euroRate);
+        Log.i(TAG,"the won_rate pushed in Bundle is = "+wonRate);
+
         //config传输数据
         config.putExtras(bdl);
         startActivityForResult(config,1);
     }
-    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){//其他页面跳转过来时的内容接收
         if(requestCode==1&&resultCode==2){
             Bundle bdl = data.getExtras();
             if(bdl!=null) {
-                dollarRate = bdl.getDouble("key_dollar", 0.0);
-                euroRate = bdl.getDouble("key_euro", 0.0);
-                wonRate = bdl.getDouble("key_won", 0.0);
+                dollarRate = bdl.getFloat("key_dollar", 0.0f);
+                euroRate = bdl.getFloat("key_euro", 0.0f);
+                wonRate = bdl.getFloat("key_won", 0.0f);
+
+                Log.i(TAG,"Bundle--------------------------------------------");
+                Log.i(TAG,"the dollar_rate get from save is = "+dollarRate);
+                Log.i(TAG,"the euro_rate get from save is = "+euroRate);
+                Log.i(TAG,"the won_rate get from save is = "+wonRate);
             }
         }
         super.onActivityResult(requestCode,resultCode,data);
     }
+    private static String inputStream2String(InputStream inputStream) throws IOException {
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
+        final StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(inputStream, "gb2312");
+        while (true) {
+            int rsz = in.read(buffer, 0, buffer.length);
+            if (rsz < 0) break;
+            out.append(buffer, 0, rsz);
+        }
+        return out.toString();
+    }
+
 }
